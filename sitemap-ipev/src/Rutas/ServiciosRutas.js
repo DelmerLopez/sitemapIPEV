969cce874e9d213c76299787304b266d9f620e32
DelmerLopez <33762845+DelmerLopez@users.noreply.github.com>
2021-02-01 17:25:38 -0600

    Adición de subsecciones de servicios

--- sitemap-ipev/src/Rutas/ServiciosRutas.js
+++ sitemap-ipev/src/Rutas/ServiciosRutas.js
@@ -2,17 +2,173 @@ const ServiciosRutas = [
     {
         id: 1,
         section: "Pensiones y jubilaciones",
-        url: "http://www.veracruz.gob.mx/ipe/pensiones-y-jubilaciones/"
+        url: "#",
+        subsections: [
+            {
+                section: "Pensiones y jubilaciones",
+                url: "http://www.veracruz.gob.mx/ipe/pensiones-y-jubilaciones/" 
+            },
+            {
+                section: "Formato de pension por causa de muerte",
+                url: "https://www.dropbox.com/s/qwr341fua6i2uj8/FORMATO_MUERTE_ACTIVO_Y_PENSIONADO_PDF.pdf" 
+            },
+            {
+                section: "Formato de pension por incapacidad",
+                url: "https://www.dropbox.com/s/t8josuzp9gsvlnu/FORMATO_JUBILACION_VEJEZ_INVALIDEZ_INCAPACIDAD_ANTICIPADA_PDF.pdf" 
+            },
+            {
+                section: "Formato de pension por invalidez",
+                url: "https://www.dropbox.com/s/t8josuzp9gsvlnu/FORMATO_JUBILACION_VEJEZ_INVALIDEZ_INCAPACIDAD_ANTICIPADA_PDF.pdf" 
+            },
+            {
+                section: "Formato de pension por jubilación",
+                url: "https://www.dropbox.com/s/t8josuzp9gsvlnu/FORMATO_JUBILACION_VEJEZ_INVALIDEZ_INCAPACIDAD_ANTICIPADA_PDF.pdf" 
+            },
+            {
+                section: "Formato de pension por vejez",
+                url: "https://www.dropbox.com/s/t8josuzp9gsvlnu/FORMATO_JUBILACION_VEJEZ_INVALIDEZ_INCAPACIDAD_ANTICIPADA_PDF.pdf" 
+            },
+            {
+                section: "Formato de pension anticipada",
+                url: "https://www.dropbox.com/s/t8josuzp9gsvlnu/FORMATO_JUBILACION_VEJEZ_INVALIDEZ_INCAPACIDAD_ANTICIPADA_PDF.pdf" 
+            }
+        ]
     },
     {
         id: 2,
         section: "Afiliación y vigencia",
-        url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/"
+        url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/",
+        subsections: [
+            {
+                section: "Afiliación y vigencia",
+                url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/" 
+            },
+            {
+                section: "Afiliación y Expedición de Credencial IPE para derechohabientes activos",
+                url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/afiliacion-y-expedicion-de-credencial-ipe/" 
+            },
+            {
+                section: "Requisitos",
+                url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/afiliacion-y-expedicion-de-credencial-ipe/" 
+            },
+            {
+                section: "Formato de afiliación o reafiliación de activos",
+                url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2019/09/SOLICITUD_DE_CREDENCIAL_ACTIVOS_2019.pdf" 
+            },
+            {
+                section: "Formato de cambio de domicilio",
+                url: "https://www.dropbox.com/s/0n0iew7lqewqs9o/CAMBIO_DE_DOMICILIO_DE_PENSIONADO_PDF.pdf" 
+            },
+            {
+                section: "Constancia de no afiliación al IPE",
+                url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/constancia-de-no-afiliacion-al-ipe/" 
+            },
+            {
+                section: "Constancia de pensionado",
+                url: "http://www.veracruz.gob.mx/ipe/afiliacion-y-vigencia/constancia-de-pensionado/" 
+            },
+            {
+                section: "Formato de notificación de correo electrónico",
+                url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/FORMATO-DE-NOTIFICACION-DE-CORREO-ELECTR%C3%93NICO.pdf" 
+            },
+            {
+                section: "Solicitud de credencial de jubilado o pensionado",
+                url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2019/09/SOLICITUD_DE_CREDENCIAL_DE_PENSIONADOS_2019.pdf" 
+            },
+            {
+                section: "Formato de fallecimiento",
+                url: "https://www.dropbox.com/s/zhoaqkuzd0v2vyt/FORMATO_DE_FALLECIMIENTO_PDF.pdf" 
+            }
+        ]
     },
     {
         id: 3,
-        section: "Prestamos",
-        url: "http://www.veracruz.gob.mx/ipe/prestamos/"
+        section: "Préstamos",
+        url: "",
+        subsections: [
+            {
+                section: "Préstamos",
+                url: "http://www.veracruz.gob.mx/ipe/prestamos/" 
+            },
+            {
+                section: "Prestamos domiciliados a corto plazo",
+                url: "",
+                subsections: [
+                    {
+                        section: "Formato para jubilados",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/PCP-Jubilado-2021.pdf" 
+                    },
+                    {
+                        section: "Formato para miembros activos",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/PCP-Activo-2021.pdf" 
+                    }
+                ]
+            },
+            {
+                section: "Prestamos domiciliados a mediano plazo",
+                url: "",
+                subsections: [
+                    {
+                        section: "Formato para jubilados",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/PMP-2021.pdf" 
+                    },
+                    {
+                        section: "Formato para miembros activos",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/PMP-2021.pdf" 
+                    }
+                ]
+            },
+            {
+                section: "Requisitos para prestamos",
+                url: "",
+                subsections: [
+                    {
+                        section: "Requisitos para activos",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/requisitos-activos-2021.pdf" 
+                    },
+                    {
+                        section: "Requisitos para jubilados",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/requisitos-pensionados-2021.pdf" 
+                    }
+                ]
+            },
+            {
+                section: "Trípticos",
+                url: "",
+                subsections: [
+                    {
+                        section: "Tríptico para solicitar préstamos",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2020/08/Tr%C3%ADptico-pr%C3%A9stamos-2020.pdf" 
+                    },
+                    {
+                        section: "Tríptico para formas de pago",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2020/08/TRIPTICO_FORMA_DE_PAGO_2020.pdf" 
+                    }
+                ]
+            },
+            {
+                section: "Formato para solicitar la domiciliación",
+                url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2021/01/Formato-de-domiciliacion.pdf" 
+            },
+            {
+                section: "Guía para llenar el formato de la domiciliación",
+                url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2019/09/GUIA_Formato_de_Domiciliacion_actu.pdf" 
+            },
+            {
+                section: "Cédulas",
+                url: "",
+                subsections: [
+                    {
+                        section: "Cédular a corto plazo",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2019/09/Cedulas_Corto_Plazo.pdf"
+                    },
+                    {
+                        section: "Cédular a mediano plazo",
+                        url: "http://www.veracruz.gob.mx/ipe/wp-content/uploads/sites/20/2019/09/Cedulas_Mediano_Plazo.pdf"
+                    }
+                ] 
+            },
+        ]
     },
     {
         id: 4,
@@ -27,7 +183,25 @@ const ServiciosRutas = [
     {
         id: 6,
         section: "Otras prestaciones",
-        url: "http://www.veracruz.gob.mx/ipe/otras-prestaciones/"
+        url: "http://www.veracruz.gob.mx/ipe/otras-prestaciones/",
+        subsections: [
+            {
+                section: "Otras prestaciones",
+                url: "http://www.veracruz.gob.mx/ipe/otras-prestaciones/"
+            },
+            {
+                section: "Gastos de funeral",
+                url: "https://www.dropbox.com/s/hgrw6s4spv119d6/FORMATO_GASTOS_DE_FUNERAL_ACTIVO_Y_PENSIONADO_PDF.pdf"
+            },
+            {
+                section: "Indemnización global",
+                url: "https://www.dropbox.com/s/4vupykct1d7dwrk/FORMATO_INDEMNIZACION_GLOBAL_PDF.pdf"
+            },
+            {
+                section: "Pagos de días y aguinaldo proporcional",
+                url: "https://www.dropbox.com/s/egolyqo9j6yub6x/FORMATO_PAGO_DE_DIAS_Y_AGUINALDO_PDF.pdf"
+            }
+        ]
     }
 ]
 
