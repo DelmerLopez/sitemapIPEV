bceab4492ec7e91bb4c0d6869b9f5d3e763f4d06
DelmerLopez <33762845+DelmerLopez@users.noreply.github.com>
2021-02-15 19:46:58 -0600

    Cambio de icono cuando se interactáa con las secciones

--- sitemap-ipev/src/Components/Section.js
+++ sitemap-ipev/src/Components/Section.js
@@ -1,15 +1,29 @@
 import { Accordion, Button,  Card} from 'react-bootstrap';
 import '../styles/section.css'
-import { ChevronDown } from 'react-bootstrap-icons';
+import { ChevronDown, ChevronUp } from 'react-bootstrap-icons';
+import { useState } from 'react';
 
 function Section(props) {
+
+    const [chevron, setChevron] = useState(<ChevronDown color="black"/>);
+    const [estadoChevron, setEstadoChevron] = useState(0);
+
+    const cambiarChevron = (e) => {
+        if (estadoChevron === 0) {
+            setChevron(<ChevronUp color="black" />);
+            setEstadoChevron(1)
+        } else {
+            setChevron(<ChevronDown color="black"/>)
+            setEstadoChevron(0)
+        }
+    }
 
     return (
         <Accordion>
             <Card>
                 <Card.Header className="card-title">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        <span className="title-text">{props.title}</span> <ChevronDown color="black"/>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0" onClick={e => cambiarChevron(e)}>
+                        <span className="title-text">{props.title}</span> {chevron}
                     </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
